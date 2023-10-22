d270e592316408f08bb884e966f11c7dda7d6183
prajjwalguhe <prajwalguhe2000@gmail.com>
2023-10-22 12:45:21 +0530

    Setup.py updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='DiamondPricePrediction',
+    version='0.0.1',
+    author="prajjwal guhe",
+    author_email='prajwalguhe2000@gmaul.com',
+    packages=find_packages()
+)
